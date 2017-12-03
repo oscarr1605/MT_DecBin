84340b043a5e04971622c1c4cb577a2210e51639
Oscar Romero <oscarmrc1605@gmail.com>
2017-12-03 16:04:07 -0600

    fix diagram and transitions

--- js/TMcode.js
+++ js/TMcode.js
@@ -479,8 +479,8 @@ function transicion(){
 		current_transition += tapes[i].get_current_symbol()+",";
 	}
 	current_transition += tapes[n_tapes - 1].get_current_symbol();
-	// console.log(current_transition);
 	console.log(transitions[current_transition]);
+	console.log('');
 
 	//If transition exists, apply transition.
 
